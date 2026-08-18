537304c87482efcecf411388c8682ad9d76808e2
Sadcardation <65435482+Sadcardation@users.noreply.github.com>
2026-08-18 05:16:27 +0000

    Deploying to gh-pages from @ Sadcardation/sadcardation.github.io@4441f4cda4bb5b40ffec82974717d3136bac58bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "Updated on August 18th 2026.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
